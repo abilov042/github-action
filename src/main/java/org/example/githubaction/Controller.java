8eff7f579d97ad6dba9d1c4f1f725b7d9dcebf97
abilov042 <shamilabilov599@gmail.com>
2024-08-01 23:50:26 +0400

    first commit

---
+++ src/main/java/org/example/githubaction/Controller.java
@@ -0,0 +1,15 @@
+package org.example.githubaction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping
+    public String getHello(){
+        return "Hello World";
+    }
+}
---
+++ src/test/java/org/example/githubaction/GithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
